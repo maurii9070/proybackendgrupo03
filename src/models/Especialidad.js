bbec3396a87d9cbaa7f5fb300760fe3e23ac2c1b
Ariel Martinez <ariel.mtz85@gmail.com>
2025-06-24 19:38:03 -0300

    Feature (modelo Doctor): se agrega modelo Doctor y Especialidad

---
+++ src/models/Especialidad.js
@@ -0,0 +1,15 @@
+// modelo para especialidad de un doctor
+import mongoose from 'mongoose';
+
+const EspecialidadSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: [true, 'El nombre de la especialidad es obligatorio'],
+    unique: true,
+    trim: true
+  }
+}, {
+  timestamps: true
+});
+
+export default mongoose.model('Especialidad', EspecialidadSchema);
