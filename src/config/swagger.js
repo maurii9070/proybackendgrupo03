af07db3051fe58d67519eba460be9e43e233598e
Mauricio Velazquez <mauri_i9070@hotmail.com>
2025-06-29 22:35:30 -0300

    Se agrega Scalar para la documentacion, mejor interfaz, pa ser distintos a to2

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,49 +1,62 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import { apiReference } from '@scalar/express-api-reference'
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API Documentation',
-            version: '1.0.0',
-            description: 'Documentación de mi API',
-        },
-        servers: [
-            //se puede agregar el servidor desplegado en producción
-            {
-                url: 'http://localhost:4000', // Cambia esto según tu entorno
-                description: 'Servidor local',
-            },
-        ],
-        //PARA AGREGAR AUTENTICACIÓN CON JWT
-        // components: {
-        //     securitySchemes: {
-        //         bearerAuth: {
-        //             type: 'http',
-        //             scheme: 'bearer',
-        //             bearerFormat: 'JWT',
-        //         },
-        //     },
-        // },
-        // security: [
-        //     {
-        //         bearerAuth: [],
-        //     },
-        // ],
-    },
-    apis: ['src/routes/*.js'], // Ruta a tus archivos de rutas
-};
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'API Documentation',
+			version: '1.0.0',
+			description: 'Documentación de mi API',
+		},
+		servers: [
+			//se puede agregar el servidor desplegado en producción
+			{
+				url: 'http://localhost:4000', // Cambia esto según tu entorno
+				description: 'Servidor local',
+			},
+		],
+		//PARA AGREGAR AUTENTICACIÓN CON JWT
+		// components: {
+		//     securitySchemes: {
+		//         bearerAuth: {
+		//             type: 'http',
+		//             scheme: 'bearer',
+		//             bearerFormat: 'JWT',
+		//         },
+		//     },
+		// },
+		// security: [
+		//     {
+		//         bearerAuth: [],
+		//     },
+		// ],
+	},
+	apis: ['src/routes/*.js'], // Ruta a tus archivos de rutas
+}
 
-const swaggerSpec = swaggerJsdoc(options);
-export const swaggerDocs = (app) => {
-    // Configurar la ruta para acceder a la documentación Swagger
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const swaggerSpec = swaggerJsdoc(options)
+export const swaggerDocs = app => {
+	// Configurar la ruta para acceder a la documentación Swagger
+	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
-    // Opcional: Ruta para obtener el spec JSON
-    app.get('/api-docs.json', (req, res) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(swaggerSpec);
-    });
-    console.log('Documentación Swagger disponible en /api-docs');
-};
+	app.use(
+		'/reference',
+		apiReference({
+			spec: {
+				content: swaggerSpec,
+			},
+			theme: 'deepSpace',
+			darkMode: true,
+			layout: 'modern',
+		})
+	)
+
+	// Opcional: Ruta para obtener el spec JSON
+	app.get('/api-docs.json', (req, res) => {
+		res.setHeader('Content-Type', 'application/json')
+		res.send(swaggerSpec)
+	})
+	console.log('Documentación Swagger disponible en /api-docs')
+}
