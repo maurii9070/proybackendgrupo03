60f6d6efe3f67646c05221ad38053d5a9533c50b
Mauricio Velazquez <mauri_i9070@hotmail.com>
2025-07-02 00:26:13 -0300

    Relacion entre archivo y turno

--- src/models/Turno.js
+++ src/models/Turno.js
@@ -1,36 +1,40 @@
-import mongoose from "mongoose";
-import Paciente from "./Paciente.js";
-import Doctor from "./Doctor.js";
+import mongoose from 'mongoose'
 
 const turnoSchema = new mongoose.Schema({
-    fecha: {
-        type: Date,
-        required: [true, "La fecha del turno es obligatoria"],
-    },
-    hora: {
-        type: String,
-        required: [true, "La hora del turno es obligatoria"],
-    },
-    paciente: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Paciente",
-        required: [true, "El paciente es obligatorio"],
-    },
-    doctor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Doctor",
-        required: [true, "El doctor es obligatorio"],
-    },
-    estado: {
-        type: String,
-        enum: ["pendiente", "realizado", "cancelado"],
-        default: "pendiente",
-    },
-    observaciones: {
-        type: String,
-        trim: true,
-        default: "",
-    }
+	fecha: {
+		type: Date,
+		required: [true, 'La fecha del turno es obligatoria'],
+	},
+	hora: {
+		type: String,
+		required: [true, 'La hora del turno es obligatoria'],
+	},
+	paciente: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Paciente',
+		required: [true, 'El paciente es obligatorio'],
+	},
+	doctor: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Doctor',
+		required: [true, 'El doctor es obligatorio'],
+	},
+	estado: {
+		type: String,
+		enum: ['pendiente', 'realizado', 'cancelado'],
+		default: 'pendiente',
+	},
+	observaciones: {
+		type: String,
+		trim: true,
+		default: '',
+	},
+	archivos: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Archivo',
+		},
+	],
 })
-const Turno = mongoose.model("Turno", turnoSchema);
-export default Turno;
+const Turno = mongoose.model('Turno', turnoSchema)
+export default Turno
