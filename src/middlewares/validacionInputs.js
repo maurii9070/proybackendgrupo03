a6e19609549be32aff11266a1ca7f30631bb33a1
Mauricio Velazquez <mauri_i9070@hotmail.com>
2025-07-02 01:45:07 -0300

    Modificacion en la validacion, ahora muestra los errores

--- src/middlewares/validacionInputs.js
+++ src/middlewares/validacionInputs.js
@@ -8,24 +8,13 @@
  * Si no hay errores, permite que la petición continúe hacia el siguiente middleware o controlador.
  */
 
-import { validationResult } from 'express-validator';
+import { validationResult } from 'express-validator'
 
 export const handleInputErrors = (req, res, next) => {
-	let errors = validationResult(req); // Obtiene los errores de validación
-	// Si hay errores, devuelve un error 400 con los detalles de los errores
-	// Si no hay errores, llama a next() para continuar con la siguiente función middleware
+	let errors = validationResult(req)
 	if (!errors.isEmpty()) {
-		const extractedErrors = [];
-		errors.array().forEach(err => {
-			// Extrae los errores y los formatea
-			if (!extractedErrors[err.path]) {
-				extractedErrors[err.path] = [];
-			}
-			extractedErrors[err.path].push(err.msg);
-		});
-
-		return res.status(400).json({ errors: extractedErrors }); // Devuelve un error 400 con los errores de validación
+		res.status(400).json({ errors: errors.array() })
+		return
 	}
-
-	next(); // Si no hay errores, continúa con la siguiente función middleware
-};
+	next()
+}
