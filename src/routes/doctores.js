b7db9ddac2f8eda4d199030b9ff37ab4ac9bdac0
RocioAV <44351449@fi.unju.edu.ar>
2025-07-04 01:09:41 -0300

    Se agrego la funcionalidad getDoctorById

--- src/routes/doctores.js
+++ src/routes/doctores.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { registrarDoctor, getDoctores, actualizarDoctor, eliminarDoctor, getDoctoresByName, getDoctoresByEspecialidad } from '../controllers/doctorController.js';
+import { registrarDoctor, getDoctores, actualizarDoctor, eliminarDoctor, getDoctoresByName, getDoctoresByEspecialidad, getDoctorById } from '../controllers/doctorController.js';
 
 // rutas
 router.post('/', registrarDoctor); // Registrar un nuevo doctor
@@ -9,6 +9,7 @@ router.put('/:id', actualizarDoctor); // Actualizar un doctor por ID
 router.delete('/:id', eliminarDoctor); // Eliminar un doctor por ID (borrado lógico)
 router.get('/name', getDoctoresByName);
 router.get('/especialidad/:idEspecialidad', getDoctoresByEspecialidad)
+router.get('/:id',getDoctorById)
 
 // Faltan agregar validaciones y middlewares de autorización 
 // las rutas que estan, ya estan probadas y funcionan correctamente
