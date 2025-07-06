bb182b614975134ac74c29d95d5eb677ac8d867a
Mauricio Velazquez <mauri_i9070@hotmail.com>
2025-07-06 00:49:42 -0300

    rutas del doctor protegidas

--- src/routes/doctores.js
+++ src/routes/doctores.js
@@ -1,17 +1,61 @@
-import express from 'express';
-const router = express.Router();
-import { registrarDoctor, getDoctores, actualizarDoctor, eliminarDoctor, getDoctoresByName, getDoctoresByEspecialidad, getDoctorById } from '../controllers/doctorController.js';
+import express from 'express'
+const router = express.Router()
+import {
+	registrarDoctor,
+	getDoctores,
+	actualizarDoctor,
+	eliminarDoctor,
+	getDoctoresByName,
+	getDoctoresByEspecialidad,
+	getDoctorById,
+} from '../controllers/doctorController.js'
+import { body, param } from 'express-validator'
+import { handleInputErrors } from '../middlewares/validacionInputs.js'
+import { autorizarRoles, protegerRuta } from '../middlewares/authMiddleware.js'
 
 // rutas
-router.post('/', registrarDoctor); // Registrar un nuevo doctor
-router.get('/', getDoctores); // Obtener todos los doctores
-router.put('/:id', actualizarDoctor); // Actualizar un doctor por ID
-router.delete('/:id', eliminarDoctor); // Eliminar un doctor por ID (borrado lógico)
-router.get('/name', getDoctoresByName);
+router.post(
+	'/',
+	body('dni').notEmpty().withMessage('El DNI es obligatorio'),
+	body('email').isEmail().withMessage('El email debe ser válido'),
+	body('password').notEmpty().withMessage('La contraseña es obligatoria'),
+	body('nombre').notEmpty().withMessage('El nombre es obligatorio'),
+	body('apellido').notEmpty().withMessage('El apellido es obligatorio'),
+	body('matricula').notEmpty().withMessage('La matrícula es obligatoria'),
+	body('especialidad').notEmpty().withMessage('La especialidad es obligatoria'),
+	body('precioConsulta').isNumeric().withMessage('El precio de la consulta debe ser un número'),
+	body('telefono').optional().isString().withMessage('El teléfono debe ser una cadena de texto'),
+	handleInputErrors,
+	protegerRuta,
+	autorizarRoles(['admin']),
+	registrarDoctor
+)
+
+// Registrar un nuevo doctor
+router.get('/', getDoctores) // Obtener todos los doctores
+
+// Actualizar un doctor por ID
+router.put(
+	'/:id',
+	param('id').isMongoId().withMessage('El ID debe ser un ID de Mongo válido'),
+	handleInputErrors,
+	protegerRuta,
+	autorizarRoles(['admin']),
+	actualizarDoctor
+)
+
+// Eliminar un doctor por ID (borrado lógico)
+router.delete(
+	'/:id',
+	param('id').isMongoId().withMessage('El ID debe ser un ID de Mongo válido'),
+	handleInputErrors,
+	protegerRuta,
+	autorizarRoles(['admin']),
+	eliminarDoctor
+)
+
+router.get('/name', getDoctoresByName)
 router.get('/especialidad/:idEspecialidad', getDoctoresByEspecialidad)
-router.get('/:id',getDoctorById)
+router.get('/:id', getDoctorById)
 
-// Faltan agregar validaciones y middlewares de autorización 
-// las rutas que estan, ya estan probadas y funcionan correctamente
-
-export default router;
+export default router
